dff1dde9b4d3f082a491d38585bbd583900b3894
Kehinde Onifade <onifkay@gmail.com>
2022-09-02 19:53:39 +0100

    Update test

--- src/test/itemsCounter.test.js
+++ src/test/itemsCounter.test.js
@@ -6,7 +6,7 @@ const fetchMock = require('fetch-mock');
 
 describe('async fetch', () => {
   test('can fetch', async () => {
-    fetchMock.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=British', {
+    fetchMock.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef', {
       meals: [{ idMeal: '123' },
         { idMeal: '123' },
         { idMeal: '123' },
@@ -16,7 +16,7 @@ describe('async fetch', () => {
         { idMeal: '123' },
       ],
     });
-    const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=British');
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef');
     const data = await response.json();
     expect(data.meals.length).toBe(7);
   });
